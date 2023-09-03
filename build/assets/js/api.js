4f7d1eb83251b21f17bbe7468eadba0312f6f739
roynaldi3301 <prasbhara0604@gmail.com>
2023-09-04 06:00:16 +0700

    gawe anyar

---
+++ build/assets/js/api.js
@@ -0,0 +1,34 @@
+
+async function fetchApiGet(endpoint, params = '') {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    let method = 'GET'
+    let url = `https://api.lolhuman.xyz/api/${endpoint}?apikey=${apik}${params}`
+    xhr.open(method, url, true)
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText)
+        resolve(data)
+      } else {
+        reject(new Error(`Kesalahan dalam mengambil data:${xhr.status} ${xhr.statusText}`))
+      }
+    }
+    xhr.onerror = () => {
+      reject(new Error(`Terjadi kesalahan dalam mengirim permintaan`))
+
+    }
+    xhr.send();
+  })
+}
+
+async function apikey() {
+  try {
+    let data = await fetchApiGet('checkapikey')
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+
+  }
+}
+
+apikey()
