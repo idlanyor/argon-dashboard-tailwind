afb98a42a0153efed6f88602a99c0e1b3bbf3fff
roynaldi3301 <prasbhara0604@gmail.com>
2023-09-04 16:13:57 +0700

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports =  {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader', {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: 'postcss-config.js'
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
